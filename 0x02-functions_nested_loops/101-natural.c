bbd76879d79452596f8b0650299c93847528b84d
Verus SIM <segunmichael24@gmail.com>
2022-09-14 21:03:29 +0000

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,21 +8,14 @@
 
 int main(void)
 {
-	int i;
-	long int fibonacci[50];
+	int i, sum = 0;
 
-	fibonacci[0] = 1;
-	fibonacci[1] = 2;
-	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
-
-	for (i = 2; i < 50; i++)
+	for (i = 0; i < 1024; i++)
 	{
-		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-		if (i == 49)
-			printf("%ld\n", fibonacci[i]);
-		else
-			printf("%ld, ", fibonacci[i]);
+		if ((i % 5) == 0 || (i % 3) == 0)
+			sum += i;
 	}
+	printf("%d\n", sum);
 
 	return (0);
 }
