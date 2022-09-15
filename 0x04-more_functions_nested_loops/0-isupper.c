184ef3bcb1b6cc57b7763240f68ed1428912ecac
Verus SIM <segunmichael24@gmail.com>
2022-09-15 20:02:57 +0000

    Project 0, more on function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - A function that checks for uppercase character.
+ * @c: An input character
+ * Return: 1 if c is uppercase or 0 otherwise
+ */
+int _isupper(int c)
+{
+	int isupper = 0;
+
+	for (char ch = 'A'; ch <= 'Z'; ch++)
+	{
+		if  (c == ch)
+		{
+			isupper = 1;
+			break;
+		}
+	}
+	return (isupper);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+More on function and nested loops
