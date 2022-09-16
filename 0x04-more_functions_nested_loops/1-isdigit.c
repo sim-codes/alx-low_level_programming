af7c6d734bc5344c1b087c73affd1d76771666d8
Verus SIM <segunmichael24@gmail.com>
2022-09-16 15:01:47 +0000

    Projects 1 & 2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isdigit - A function that checks for a digit (0 through 9).
+ * @c: An input integer
+ * Return: 1 if c is a digit or 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	char i;
+	int isdigit = 0;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (c == i)
+		{
+			isdigit = 1;
+			break;
+		}
+	}
+	return (isdigit);
+
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - A function that multiplies 2 numbers.
+ * @a: First integer
+ * @b: Second integer
+ * Return: returns their multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
