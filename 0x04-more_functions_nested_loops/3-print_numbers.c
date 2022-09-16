427ab6d38e319a7b255888495eda6021acd8cc9c
Verus SIM <segunmichael24@gmail.com>
2022-09-16 15:17:27 +0000

    Projects 3 & 4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * void print_numbers - A function that prints numbers from 0 to 9
+ * _putchar - prints each character out
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
