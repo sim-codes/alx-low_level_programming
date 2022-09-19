1c163d54ea9e593fd9fd36bad0d556d33815c64e
Verus SIM <segunmichael24@gmail.com>
2022-09-19 23:17:14 +0000

    Correction

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,11 +4,14 @@
  * _puts - A function that prints string to stdout
  * @str: Value to be printed
  */
-int main(void)
+void _puts(char *str)
 {
-  char *str;
+	int i = 0;
 
-  str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-  _puts(str);
-  return (0);
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
