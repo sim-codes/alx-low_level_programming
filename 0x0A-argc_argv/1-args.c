84a3952d8a82130010542b8db64a896bc78af5ba
Verus SIM <segunmichael24@gmail.com>
2022-10-12 14:14:22 +0000

    corrections

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * main - A program that prints the number of arguements you passed into it
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * main - A program that prints all arguements it receives
